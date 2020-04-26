3d215d77922cb7b14b59fffdf38fb0780b50cdbb
Eugene Luo <eyluo@andrew.cmu.edu>
2020-04-26 12:33:30 -0400

    starting work on track.py

--- spotifython/track.py
+++ spotifython/track.py
@@ -3,12 +3,21 @@ from artist import Artist
 
 class Track:
     def __init__(self, track_info: dict):
-        self.album = track_info.get('album', None)
-        self.artists = list()
+        self._album = Album(track_info.get('album', None))
+        self._artists = list()
         for artist in track_info.get('artists', None):
-            self.artists.append(Artist(artist))
-        self.available_markets = track_info.get('available_markets', None)
-        self.id = track_info.get('id', None)
-        self.images = track_info.get('images', None)
+            self._artists.append(Artist(artist))
+        
+        self._raw = track_info
 
-        self.raw = track_info
+    def album(self):
+        return self._album
+
+    def artists(self):
+        return self._artists
+
+    def available_markets(self):
+        return self._raw['available_markets']
+
+    def asdict(self):
+        return self._raw
