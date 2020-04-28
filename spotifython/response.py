915f0420162ea6758ed0b308b75d7e46754fa183
Eugene Luo <eyluo@andrew.cmu.edu>
2020-04-28 19:00:07 -0400

    added response object

---
+++ spotifython/response.py
@@ -0,0 +1,23 @@
+class Response:
+    OK = 'OK'
+    # TODO: add more appropriate response codes
+
+    def __init__(self, status, contents : object):
+        self._status = status
+        self._contents = contents
+
+    def status(self):
+        return self._status
+
+    def contents(self):
+        return self._contents
+
+
+# user code
+
+# response = album.tracks()
+# if response.status() != Response.OK:
+#     print('this is bad uh oh')
+
+
+# tracks = response.contents()
