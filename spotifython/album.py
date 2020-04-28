5f7efa620ab6563b8e5432f76868771be7a4e8c8
Eugene Luo <eyluo@andrew.cmu.edu>
2020-04-27 23:35:32 -0400

    rough draft of track and album objects

--- spotifython/album.py
+++ spotifython/album.py
@@ -1,4 +1,8 @@
 from track import Track
+from copy import deepcopy
+
+
+TRACKS = 'GET https://api.spotify.com/v1/albums/%s/tracks'
 
 class Album:
     def __init__(self, album_info):
@@ -6,6 +10,22 @@ class Album:
         self.artists = list()
         for artist in album_info.get('artists', []):
             self.artists.append(Artist(artist))
-        self.available_markets = album_info.get('available_markets', [])
-        self.id = album_info.get('id', '')
-        self.images = album_info.get('images', '')
+
+        self._raw = album_info
+
+    def __str__(self):
+        return '%s by %s' % (self._raw['name'], self._artists[0].name())
+
+    def __eq__(self, other):
+        return (isinstance(other, Album) and self._raw == other._raw)
+
+    # TODO: these will be getter methods to fetch things from _raw. Boilerplate,
+    # so I will address in a wee bit.
+
+    def tracks(self):
+        if 'id' not in self._raw:
+            return None
+        album_id = self._raw.get['id']
+        request = TRACKS % (album_id)
+        # TODO: send the actual request
+        return
