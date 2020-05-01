9172ecc54d2f73b1bc8e9d5e3e487021ca82a052
Spencer Yu <syu04012012@gmail.com>
2020-05-01 16:06:06 -0400

    update style: documentation should be inside function definition. updated comma separated lists to be constants. updated from_token to be constant.

--- spotifython/Spotifython.py
+++ spotifython/Spotifython.py
@@ -3,21 +3,30 @@ from response import *
 # This object should be constructed by the user to instantiate the 
 # session of Spotify Web API usage.
 class Spotifython:
+
+    # Constants
+    FROM_TOKEN = 'from_token'
+    ALBUM = 'album'
+    ARTIST = 'artist'
+    PLAYLIST = 'playlist'
+    TRACK = 'track'
+    SHOW = 'show'
+    EPISODE = 'episode'
     
     def __init__(self, token, timeout=None):
         self.token = token
         self.timeout = timeout
     
-    '''
-    Updates the stored Spotify authentication token for this instance.
-
-    Args:
-        token: the new Spotify authentication token.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-    '''
     def reauthenticate(self, token: str):
+        '''
+        Updates the stored Spotify authentication token for this instance.
+
+        Args:
+            token: the new Spotify authentication token.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+        '''
         self.token = token
 
     # User should never call this constructor. As a result, they should never
@@ -65,46 +74,46 @@ class Spotifython:
     # API Calls
     ##################################
 
-    '''
-    Searches for content with the given query.
-
-    Args:
-        query: search query keywords and optional field filters and operators.
-        type: a comma-separated list of the types of results to search for.
-            Valid arguments are "album", "artist", "playlist", "track".
-            Note: shows and episodes are not supported in this version.
-        search_limit: the maximum number of results to return.
-        market: (Optional) An ISO 3166-1 alpha-2 country code or the string 
-            from_token. If a country code is specified, only artists, albums,
-            and tracks with content that is playable in that market is returned.
-            Note:
-            - Playlist results are not affected by the market parameter.
-            - If market is set to from_token, and a valid access token is 
-            specified in the request header, only content playable
-            in the country associated with the user account, is returned.
-        include_external: (Optional) Valid argument is "audio": if specified,
-            the response will include any relevant audio content that is 
-            hosted externally. By default external content is filtered out 
-            from responses.
-
-    Returns:
-        A response object containing a SearchResult if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError if query type, market, or include_external is invalid.
-        ValueError if search_limit is > 2000: this is the Spotify API's search limit.
-
-    Calls endpoints: 
-        GET   /v1/search
-    ''' 
     def search(self, 
         query: str, 
-        type: str, # album,artist,playlist,track,show,episode (comma separated)
+        type: Union[str, List[str]],
         search_limit: int,
         market: str = None,
         include_external: str = None # audio
     ) -> Response: # SearchResult
+        '''
+        Searches for content with the given query.
+
+        Args:
+            query: search query keywords and optional field filters and operators.
+            type: a comma-separated list of the types of results to search for.
+                Valid arguments are sp.ALBUM, sp.ARTIST, sp.PLAYLIST, and sp.TRACK.
+                Note: shows and episodes are not supported in this version.
+            search_limit: the maximum number of results to return.
+            market: (Optional) An ISO 3166-1 alpha-2 country code or the string 
+                sp.FROM_TOKEN. If a country code is specified, only artists, albums,
+                and tracks with content that is playable in that market is returned.
+                Note:
+                - Playlist results are not affected by the market parameter.
+                - If market is set to sp.FROM_TOKEN, and a valid access token is 
+                specified in the request header, only content playable
+                in the country associated with the user account, is returned.
+            include_external: (Optional) Valid argument is "audio": if specified,
+                the response will include any relevant audio content that is 
+                hosted externally. By default external content is filtered out 
+                from responses.
+
+        Returns:
+            A response object containing a SearchResult if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError if query type, market, or include_external is invalid.
+            ValueError if search_limit is > 2000: this is the Spotify API's search limit.
+
+        Calls endpoints: 
+            GET   /v1/search
+        ''' 
 
         # Don't forget to encode the spaces in strings!
         # See guidelines in Search -> "Writing a Query - Guidelines" 
@@ -113,144 +122,145 @@ class Spotifython:
         # Search limit is internally represented using API calls with params:
         #    limit: int = None,
         #    offset: int = None,
-        # It is a required field due to the offset + limit being 2000, which would take 40 backend API calls.
+        # It is a required field due to the offset + limit being 2000, which would take
+        # 40 backend API calls.
         # Throw an error if > 2000.
         
         return Response(status=Response.OK, contents=SearchResult(dict()))
 
-    '''
-    Gets the albums with the given Spotify album ids.
-
-    Args:
-        album_ids: a string or list of strings of the Spotify album ids to search for.
-        market: (Optional) An ISO 3166-1 alpha-2 country code or the string 'from_token'. 
-            Provide this parameter if you want to apply Track Relinking.
-
-    Returns:
-        A response object containing an Album or List[Album] if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError if market type is invalid.
-
-    Calls endpoints: 
-        GET   /v1/albums/{id}
-        GET   /v1/albums
-    ''' 
     def get_albums(self, 
-        album_ids: Union[str, List[str]], # If list, is comma separated
+        album_ids: Union[str, List[str]],
         market: str = None
     ) -> Response: # Union[Album, List[Album]]
+        '''
+        Gets the albums with the given Spotify album ids.
+
+        Args:
+            album_ids: a string or list of strings of the Spotify album ids to search for.
+            market: (Optional) An ISO 3166-1 alpha-2 country code or the string sp.FROM_TOKEN. 
+                Provide this parameter if you want to apply Track Relinking.
+
+        Returns:
+            A response object containing an Album or List[Album] if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError if market type is invalid.
+
+        Calls endpoints: 
+            GET   /v1/albums/{id}
+            GET   /v1/albums
+        ''' 
         return None
 
-    '''
-    Gets the artists with the given Spotify artists ids.
-
-    Args:
-        artist_ids: a string or list of strings of the Spotify artist ids to search for.
-
-    Returns:
-        A response object containing an Artist or List[Artists] if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-
-    Calls endpoints: 
-        GET   /v1/artists/{id}
-        GET   /v1/artists
-    ''' 
     def get_artists(self, 
-        artist_ids: Union[str, List[str]] # If list, is comma separated
+        artist_ids: Union[str, List[str]]
     ) -> Response: # Union[Artist, List[Artist]]
+        '''
+        Gets the artists with the given Spotify artists ids.
+
+        Args:
+            artist_ids: a string or list of strings of the Spotify artist ids to search for.
+
+        Returns:
+            A response object containing an Artist or List[Artists] if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+
+        Calls endpoints: 
+            GET   /v1/artists/{id}
+            GET   /v1/artists
+        ''' 
         return None
 
-    '''
-    Gets the tracks with the given Spotify track ids.
-
-    Args:
-        track_ids: a string or list of strings of the Spotify track ids to search for.
-        market: (Optional) An ISO 3166-1 alpha-2 country code or the string 'from_token'. 
-            Provide this parameter if you want to apply Track Relinking.
-
-    Returns:
-        A response object containing a Track or List[Track] if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError if market type is invalid.
-
-    Calls endpoints: 
-        GET   /v1/tracks/{id}
-        GET   /v1/tracks
-    ''' 
     def get_tracks(self, 
-        track_ids: Union[str, List[str]], # If list, is comma separated
+        track_ids: Union[str, List[str]], 
         market: str = None
     ) -> Response: # Union[Track, List[Track]]
+        '''
+        Gets the tracks with the given Spotify track ids.
+
+        Args:
+            track_ids: a string or list of strings of the Spotify track ids to search for.
+            market: (Optional) An ISO 3166-1 alpha-2 country code or the string sp.FROM_TOKEN. 
+                Provide this parameter if you want to apply Track Relinking.
+
+        Returns:
+            A response object containing a Track or List[Track] if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError if market type is invalid.
+
+        Calls endpoints: 
+            GET   /v1/tracks/{id}
+            GET   /v1/tracks
+        ''' 
         return None
     
-    '''
-    Gets the users with the given Spotify user ids.
-
-    Args:
-        user_ids: a string or list of strings of the Spotify user id to search for.
-    
-    Returns: A response object containing a User or List[User] if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-    
-    Calls endpoints:
-        GET	/v1/users/{user_id}
-    '''
     def get_users(self, 
         user_ids: Union[str, List[str]]
     ) -> Response: # Union[User, List[User]]
+        '''
+        Gets the users with the given Spotify user ids.
+
+        Args:
+            user_ids: a string or list of strings of the Spotify user id to search for.
+        
+        Returns: A response object containing a User or List[User] if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+        
+        Calls endpoints:
+            GET	/v1/users/{user_id}
+        '''
         return None
     
-    '''
-    Gets the user associated with the current Spotify API authentication key.
-    
-    Returns: 
-        A response object containing a User if the request succeeded.
-
-    Exceptions:
-        ValueError if the Spotify API key is not valid. TODO: is this ok
-    
-    Calls endpoints:
-        GET	/v1/me
-    '''
     def get_current_user(self) -> Response: # User
+        '''
+        Gets the user associated with the current Spotify API authentication key.
+        
+        Returns: 
+            A response object containing a User if the request succeeded.
+
+        Exceptions:
+            ValueError if the Spotify API key is not valid. TODO: is this ok
+        
+        Calls endpoints:
+            GET	/v1/me
+        '''
         return None
     
-    '''
-    Gets the tracks with the given Spotify playlist ids.
-
-    Args:
-        playlist_ids: a string or list of strings of the Spotify playlist ids to search for.
-        fields: (Optional) Filters for the query: a comma-separated list of the fields to return. 
-            If omitted, all fields are returned. A dot separator can be used to specify 
-            non-reoccurring fields, while parentheses can be used to specify reoccurring 
-            fields within objects. Use multiple parentheses to drill down into nested objects. 
-            Fields can be excluded by prefixing them with an exclamation mark.
-        market: (Optional) An ISO 3166-1 alpha-2 country code or the string 'from_token'. 
-            Provide this parameter if you want to apply Track Relinking.
-
-    Returns:
-        A response object containing a Playlist or List[Playlist] if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError if market type is invalid.
-
-    Calls endpoints:
-        GET	/v1/playlists/{playlist_id}
-    '''
-    def get_playlist(self,
+    def get_playlists(self,
         playlist_ids: Union[str, List[str]],
         fields: str = None,
         market: str = None
     ) -> Response: # Union[Playlist, List[Playlist]]
+        '''
+        Gets the tracks with the given Spotify playlist ids.
+
+        Args:
+            playlist_ids: a string or list of strings of the Spotify playlist ids to search for.
+            fields: (Optional) Filters for the query: a comma-separated list of the fields to return. 
+                If omitted, all fields are returned. A dot separator can be used to specify 
+                non-reoccurring fields, while parentheses can be used to specify reoccurring 
+                fields within objects. Use multiple parentheses to drill down into nested objects. 
+                Fields can be excluded by prefixing them with an exclamation mark.
+            market: (Optional) An ISO 3166-1 alpha-2 country code or the string sp.FROM_TOKEN. 
+                Provide this parameter if you want to apply Track Relinking.
+
+        Returns:
+            A response object containing a Playlist or List[Playlist] if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError if market type is invalid.
+
+        Calls endpoints:
+            GET	/v1/playlists/{playlist_id}
+        '''
 
         # Note: additional_types is also a valid request param - it 
         # has been deprecated and therefore is removed from the API wrapper.
--- spotifython/artist.py
+++ spotifython/artist.py
@@ -44,84 +44,88 @@ class Artist:
     # API Calls
     ##################################
     
-    '''
-    Gets the albums associated with the current Spotify artist.
-    
-    Args:
-        search_limit: (Optional) the maximum number of results to return.
-        include_groups: (Optional) A comma-separated list of keywords 
-            that will be used to filter the response. If not supplied, 
-            all album types will be returned. 
-            Valid values are: 'album', 'single', 'appears_on', 'compilation' 
-        country: (Optional) An ISO 3166-1 alpha-2 country code or the string from_token.
-            Supply this parameter to limit the response to one particular geographical 
-            market. If not given, results will be returned for all countries and you 
-            are likely to get duplicate results per album, one for each country in 
-            which the album is available!
-
-    Returns: 
-        A response object containing a list of Albums if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-    
-    Calls endpoints:
-        GET	/v1/artists/{id}/albums
-    '''
     def albums(self, 
         search_limit: int = None,
         include_groups: str = None,
         country: str = None, # By default, returns for all countries
     ) -> Response: # List[Album]
+        '''
+        Gets the albums associated with the current Spotify artist.
+        
+        Args:
+            search_limit: (Optional) the maximum number of results to return.
+            include_groups: (Optional) A comma-separated list of keywords 
+                that will be used to filter the response. If not supplied, 
+                all album types will be returned. 
+                Valid values are: 'album', 'single', 'appears_on', 'compilation' 
+            country: (Optional) An ISO 3166-1 alpha-2 country code or the string sp.FROM_TOKEN.
+                Supply this parameter to limit the response to one particular geographical 
+                market. If not given, results will be returned for all countries and you 
+                are likely to get duplicate results per album, one for each country in 
+                which the album is available!
+
+        Returns: 
+            A response object containing a list of Albums if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+        
+        Calls endpoints:
+            GET	/v1/artists/{id}/albums
+        '''
+
         # search_limit -> limit: int = None, offset: int = None
         # Request: the response body contains an array of simplified album
         # objects (wrapped in a paging object)
         return self.albums
     
-    '''
-    Gets the top tracks associated with the current Spotify artist.
     
-    Args:
-        country: An ISO 3166-1 alpha-2 country code or the string 'from_token'.
-        search_limit: (Optional) the maximum number of results to return.
-
-    Returns: 
-        A response object containing a list of Tracks if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError for invalid country.
-        ValueError if search_limit is > 10: this is the Spotify API's search limit.
-    
-    Calls endpoints:
-        GET	/v1/artists/{id}/top-tracks
-    '''
     def top_tracks(self,
         country: str,
         search_limit: int = 10,
     ) -> Response: # List[Track]
+        '''
+        Gets the top tracks associated with the current Spotify artist.
+        
+        Args:
+            country: An ISO 3166-1 alpha-2 country code or the string sp.FROM_TOKEN.
+            search_limit: (Optional) the maximum number of results to return.
+
+        Returns: 
+            A response object containing a list of Tracks if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError for invalid country.
+            ValueError if search_limit is > 10: this is the Spotify API's search limit.
+        
+        Calls endpoints:
+            GET	/v1/artists/{id}/top-tracks
+        '''
+
         # This search limit is not part of the API, spotify always returns up to 10.
         return self.top_tracks
     
-    '''
-    Gets Spotify catalog information about artists similar to a given artist.
-
-    Args:
-        search_limit: (Optional) the maximum number of results to return.
-
-    Returns: 
-        A response object containing a list of Artist objects 
-        if the request succeeded.
-
-    Exceptions:
-        TypeError for invalid types in any argument.
-        ValueError if search_limit is > 20: this is the Spotify API's search limit.
-    
-    Calls endpoints:
-        GET	/v1/artists/{id}/related-artists
-    '''
     def related_artists(self,
         search_limit: int = 20,
     ) -> Response: # List[Artist]
+        '''
+        Gets Spotify catalog information about artists similar to a given artist.
+
+        Args:
+            search_limit: (Optional) the maximum number of results to return.
+
+        Returns: 
+            A response object containing a list of Artist objects 
+            if the request succeeded.
+
+        Exceptions:
+            TypeError for invalid types in any argument.
+            ValueError if search_limit is > 20: this is the Spotify API's search limit.
+        
+        Calls endpoints:
+            GET	/v1/artists/{id}/related-artists
+        '''
+
         # This search limit is not part of the API, spotify always returns up to 20.
         return self.related_artists
