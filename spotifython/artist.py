bc54a0580d0fdf500451bab5345026293119ddcd
Spencer Yu <syu04012012@gmail.com>
2020-05-01 00:16:36 -0400

    add documentation to artist and top level class. modify methods to better fit usage. add search limit and default values.

--- spotifython/artist.py
+++ spotifython/artist.py
@@ -1,42 +1,127 @@
 from album import Album
 from track import Track
 from copy import deepcopy
+from response import *
 
 class Artist:
     # User should never call this constructor. As a result, they should never
     # have access to the artist_info structure prior to creating an Artist.
     def __init__(self, artist_info: dict):
         self._raw = artist_info
-        # External url: unsupported object -> leave alone for now
-        # Followers: unsupported object -> leave alone for now
-        self.genres = artist_info.get("genres", []) # List[str]
-        self.href = artist_info.get("href", None) # str
-        self.id = artist_info.get("id", None) # str
-        # Images: unsupported object -> leave alone for now
-        self.name = artist_info.get("name", None) # str
-        self.popularity = artist_info.get("popularity", None) # int
-        # type: is an artist. we don't need to include this.
-        self.uri = artist_info.get("uri", None) # str
-
-        # Results of API calls
         self.albums = None # Lazily load
         self.top_tracks = None # Lazily load
         self.related_artists = None # Lazily load
 
+    ##################################
+    # Field accessors
+    ##################################
+
+    ### Unsupported objects ###
+    # External url: unsupported object
+    # Followers: unsupported object
+    # Images: unsupported object
+    # Type: is an artist. we don't need to include this.
+
+    def genres(self) -> Response: # List[str]
+        return Response(status=Response.OK, contents=self._raw.get('genres'))
+
+    def href(self) -> Response: # str
+        return Response(status=Response.OK, contents=self._raw.get('href'))
+    
+    def artist_id(self) -> Response: # str
+        return Response(status=Response.OK, contents=self._raw.get('id'))
+    
+    def name(self) -> Response: # str
+        return Response(status=Response.OK, contents=self._raw.get('name'))
+    
+    def popularity(self) -> Response: # int
+        return Response(status=Response.OK, contents=self._raw.get('popularity'))
+    
+    def uri(self) -> Response: # str
+        return Response(status=Response.OK, contents=self._raw.get('uri'))
+
+    ##################################
     # API Calls
-    # return <field> if <field> else <make API call and populate field>
+    ##################################
+    
+    '''
+    Gets the albums associated with the current Spotify artist.
+    
+    Args:
+        search_limit: (Optional) the maximum number of results to return.
+        include_groups: (Optional) A comma-separated list of keywords 
+            that will be used to filter the response. If not supplied, 
+            all album types will be returned. 
+            Valid values are: 'album', 'single', 'appears_on', 'compilation' 
+        country: (Optional) An ISO 3166-1 alpha-2 country code or the string from_token.
+            Supply this parameter to limit the response to one particular geographical 
+            market. If not given, results will be returned for all countries and you 
+            are likely to get duplicate results per album, one for each country in 
+            which the album is available!
+
+    Returns: 
+        A response object containing a list of Albums if the request succeeded.
+
+    Exceptions:
+        TypeError for invalid types in any argument.
+    
+    Calls endpoints:
+        GET	/v1/artists/{id}/albums
+    '''
     def albums(self, 
+        search_limit: int = None,
         include_groups: str = None,
-        country: str = None,
-        limit: int = None,
-        offset: int = None
-    ) -> List[Album]:
+        country: str = None, # By default, returns for all countries
+    ) -> Response: # List[Album]
+        # search_limit -> limit: int = None, offset: int = None
+        # Request: the response body contains an array of simplified album
+        # objects (wrapped in a paging object)
         return self.albums
     
+    '''
+    Gets the top tracks associated with the current Spotify artist.
+    
+    Args:
+        country: An ISO 3166-1 alpha-2 country code or the string 'from_token'.
+        search_limit: (Optional) the maximum number of results to return.
+
+    Returns: 
+        A response object containing a list of Tracks if the request succeeded.
+
+    Exceptions:
+        TypeError for invalid types in any argument.
+        ValueError for invalid country.
+        ValueError if search_limit is > 10: this is the Spotify API's search limit.
+    
+    Calls endpoints:
+        GET	/v1/artists/{id}/top-tracks
+    '''
     def top_tracks(self,
-        country: str = "from_token"
-    ) -> List[Track]:
+        country: str,
+        search_limit: int = 10,
+    ) -> Response: # List[Track]
+        # This search limit is not part of the API, spotify always returns up to 10.
         return self.top_tracks
     
-    def related_artists(self) -> List[Artist]:
+    '''
+    Gets Spotify catalog information about artists similar to a given artist.
+
+    Args:
+        search_limit: (Optional) the maximum number of results to return.
+
+    Returns: 
+        A response object containing a list of Artist objects 
+        if the request succeeded.
+
+    Exceptions:
+        TypeError for invalid types in any argument.
+        ValueError if search_limit is > 20: this is the Spotify API's search limit.
+    
+    Calls endpoints:
+        GET	/v1/artists/{id}/related-artists
+    '''
+    def related_artists(self,
+        search_limit: int = 20,
+    ) -> Response: # List[Artist]
+        # This search limit is not part of the API, spotify always returns up to 20.
         return self.related_artists
