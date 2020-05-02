38b7f4a81b568f50e04eb43cb2136f36d7a7d471
Niko Gupta <niko.gupta@gmail.com>
2020-05-02 18:59:54 -0400

    Fix problems outlined in PR #23 for User class

--- spotifython/user.py
+++ spotifython/user.py
@@ -8,17 +8,25 @@ from track import Track
 from response import Response
 from typing import Union, List, Dict
 
-# TODO: exceptions for each param
 # TODO: auth required for each param
 # TODO: what to do about partial success?
 class User():
+    ''' Define behaviors related to a user, such as reading / modifying the
+        library and following artists.
+
+    Getting an instance of User should be done using spotifython.get_users()
+
+    Exceptions:
+        TypeError:  if incorrectly typed parameters are given.
+        ValueError: if parameters with illegal values are given.
+    '''
 
 
     def __init__(self,
                  sp_obj: Spotifython,
                  user_id: str,
                  known_vals: Dict=None
-        ) -> User:
+    ) -> User:
         # note to self: init self.player
         pass
 
@@ -29,12 +37,12 @@ class User():
 
 
     def _update_internal(self,
-                         known_vals: Dict
-        ) -> None:
+                         new_vals: Dict
+    ) -> None:
         ''' Used internally to keep cached data up to date
 
         Keyword arguments:
-            known_vals: a dictionary with fields that should be added to or
+            new_vals: a dictionary with fields that should be added to or
                 updated in the internal cache. Any values in the dictionary will
                 become the new value for that key.
 
@@ -57,22 +65,22 @@ class User():
         pass
 
 
-    def top(self
+    def top(self,
             top_type: str,
-            time_range: str=sp.MEDIUM,
-            limit: int=50
-        ) -> Response: # Union[List[Artist], List[Track]]
+            limit: int,
+            time_range: str=sp.MEDIUM
+    ) -> Response: # Union[List[Artist], List[Track]]
         ''' Get the top artists or tracks for the user over a time range.
 
         Keyword arguments:
             top_type: only get items of this type. One of:
                 sp.ARTIST
                 sp.TRACK
+            limit: max number of items to return.
             time_range: (optional) only get items for this time range. One of:
                 sp.LONG (several years)
                 sp.MEDIUM (about 6 months)
                 sp.SHORT (about 4 weeks)
-            limit: (optional) max number of items to return.
 
         response.contents():
             Success: list of artists or a list of tracks, depending on top_type.
@@ -87,7 +95,7 @@ class User():
 
     def recently_played(self,
                         limit: int=50
-        ) -> Response: # List[Track]
+    ) -> Response: # List[Track]
         ''' Get the user's recently played tracks
 
         Keyword arguments:
@@ -98,6 +106,9 @@ class User():
             Success: a list of tracks. Could be empty.
             Failure: None
 
+        Exceptions:
+            ValueError: raised if limit > 50
+
         Note: the 'before' and 'after' functionalities are not supported.
         '''
         # GET /v1/me/player/recently-played
@@ -106,7 +117,7 @@ class User():
 
     def get_playlists(self,
                       limit: int=None
-        ) -> Response: # List[Playlist]
+    ) -> Response: # List[Playlist]
         ''' Get all playlists that this user has in their library
 
         Keyword arguments:
@@ -126,27 +137,29 @@ class User():
         pass
 
 
-    # TODO: should this be an enum instead of 2 boolean flags?
     def create_playlist(self,
-                        name: str
-                        public: bool=True
-                        collaborative: bool=False
+                        name: str,
+                        visibility: str=sp.PUBLIC,
                         description: str=""
-        ) -> Response: # None
+    ) -> Response: # None
         ''' Create a new playlist owned by the current user
 
         Keyword arguments:
             name: The name for the new playlist. Does not need to be unique;
                 a user may have several playlists with the same name.
-            public: (optional) if the playlist should be publicly viewable.
-            collaborative: (optional) if the playlist should be collaborative.
+            visibility: (optional) describes how other users can interact with
+                this playlist. One of:
+                    sp.PUBLIC: publicly viewable, not collaborative
+                    sp.PRIVATE: not publicly viewable, not collaborative
+                    sp.PRIVATE_COLLAB: not publicly viewable, collaborative
             description: (optional) viewable description of the playlist.
 
         response.contents():
             None
 
         Exceptions:
-            ValueError: raised if both collaborative and public are True
+            TypeError: raised if visibility is not one of the types described
+                above.
         '''
         # POST /v1/users/{user_id}/playlists
         pass
@@ -155,7 +168,7 @@ class User():
     def is_following(self,
                      other: Union[Artist, User, Playlist,
                                   List[Union[Artist, User, Playlist]]]
-        ) -> Response: # List[Tuple[Union[Artist, User, Playlist], bool]]
+    ) -> Response: # List[Tuple[Union[Artist, User, Playlist], bool]]
         ''' Check if the current user is following something
 
         Keyword arguments:
@@ -164,6 +177,9 @@ class User():
                 If other is a list, it can conatin multiple types.
 
                 Can only check for Artist, User, and Playlist.
+
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
 
         response.contents():
             Success: List of tuples. Each tuple has an input object and whether
@@ -179,7 +195,7 @@ class User():
     def get_following(self,
                       follow_type: str,
                       limit: int=None
-        ) -> Response: # Union[List[Artist], List[Playlist]]
+    ) -> Response: # Union[List[Artist], List[Playlist]]
         ''' Get all follow_type objects the current user is following
 
         Keyword arguments:
@@ -192,7 +208,7 @@ class User():
             Failure: None
 
         Exceptions:
-            ValueError: if sp.USER is passed in. The Spotify web api does not
+            TypeError: if sp.USER is passed in. The Spotify web api does not
                 currently allow you to access this information.
                 For more info: https://github.com/spotify/web-api/issues/4
         '''
@@ -201,11 +217,10 @@ class User():
         pass
 
 
-    # TODO: what if already following? Should it return success or error?
     def follow(self,
                other: Union[Artist, User, Playlist,
                             List[Union[Artist, User, Playlist]]]
-        ) -> Response: # None
+    ) -> Response: # None
         ''' Follow one or more things
 
         Keyword arguments:
@@ -215,6 +230,12 @@ class User():
 
                 Can only follow Artist, User, and Playlist.
 
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
+
+        Note: if user is already following other, will do nothing and return
+            a success code in response.status()
+
         response.contents():
             None
         '''
@@ -223,11 +244,10 @@ class User():
         pass
 
 
-    # TODO: what if already not following? Should it return success or error?
     def unfollow(self,
                  other: Union[Artist, User, Playlist,
                               List[Union[Artist, User, Playlist]]]
-        ) -> Response: # None
+    ) -> Response: # None
         ''' Unfollow one or more things
 
         Keyword arguments:
@@ -236,6 +256,12 @@ class User():
                 If other is a list, it can conatin multiple types.
 
                 Can only unfollow Artist, User, and Playlist.
+
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
+
+        Note: if user is already not following other, will do nothing and return
+            a success code in response.status()
 
         response.contents():
             None
@@ -248,7 +274,7 @@ class User():
     def has_saved(self,
                   other: Union[Track, Album,
                                List[Union[Track, Album]]]
-        ) -> Response: # List[Tuple[Union[Track, Album], bool]]
+    ) -> Response: # List[Tuple[Union[Track, Album], bool]]
         ''' Check if the user has one or more things saved to their library
 
         Keyword arguments:
@@ -257,6 +283,9 @@ class User():
                 If other is a list, it can conatin multiple types.
 
                 Can only check for Track and Album.
+
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
 
         response.contents():
             Success: List of tuples. Each tuple has an input object and whether
@@ -271,7 +300,7 @@ class User():
     def get_saved(self,
                   saved_type: str,
                   limit: int=None
-        ) -> Response: # Union[List[Album], List[Track]]
+    ) -> Response: # Union[List[Album], List[Track]]
         ''' Get all saved_type objects the user has saved to their library
 
         Keyword arguments:
@@ -288,11 +317,10 @@ class User():
         pass
 
 
-    # TODO: what if already saved? Should it return success or error?
     def save(self,
              other: Union[Track, Album,
                           List[Union[Track, Album]]]
-        ) -> Response: # None
+    ) -> Response: # None
         ''' Save one or more things to the user's library
 
         Keyword arguments:
@@ -302,6 +330,12 @@ class User():
 
                 Can only save Track or Album.
 
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
+
+        Note: if user already has other saved, will do nothing and return
+            a success code in response.status()
+
         response.contents():
             None
         '''
@@ -310,11 +344,10 @@ class User():
         pass
 
 
-    # TODO: what if already removed? Should it return success or error?
     def remove(self,
                other: Union[Track, Album,
                             List[Union[Track, Album]]]
-        ) -> Response: # None
+    ) -> Response: # None
         ''' Remove one or more things from the user's library
 
         Keyword arguments:
@@ -323,6 +356,12 @@ class User():
                 If other is a list, it can conatin multiple types.
 
                 Can only remove Track or Album.
+
+        Exceptions:
+            TypeError: raised if other is not one of the types described above.
+
+        Note: if user already does not have other saved, will do nothing and
+            return a success code in response.status()
 
         response.contents():
             None
