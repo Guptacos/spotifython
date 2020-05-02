abd2fc00ea5a84979467945d9d052afc501029f9
Rama Mannava <rama.mannava@yahoo.com>
2020-05-02 03:08:58 -0400

    update playlist interface

--- spotifython/playlist.py
+++ spotifython/playlist.py
@@ -2,8 +2,6 @@ from user import User
 from track import Track
 from copy import deepcopy
 
-# TODO should we clarify that indices need to be less than len
-# TODO convenience methods like str and len?
 # objects created in this constructor
 class Playlist:
     """
@@ -38,7 +36,7 @@ class Playlist:
                   playlist instead.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
@@ -48,10 +46,10 @@ class Playlist:
         Update the playlist name.
 
         Parameters:
-        name: A string containing the new name.
+        name: A string containing the new name of this playlist.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
@@ -61,29 +59,24 @@ class Playlist:
         Update the playlist description.
 
         Parameters:
-        description: A string containing the new description.
+        description: A string containing the new description of this playlist.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
     # PUT https://api.spotify.com/v1/playlists/{playlist_id}
-    # TODO enum: private+collaborative, private+not collaborative, public
-    def update_visibility(self, public, collaborative=False):
+    def update_visibility(self, visibility):
         """
         Update the playlist public/private visibility and collaborative access.
 
         Parameters:
-        public:        A boolean value marking the playlist as public. Set to
-                       False to mark the playlist as private.
-
-        Optional Parameters:
-        collaborative: A boolean value marking the playlist as collaborative.
-                       A playlist can be collaborative only if it is not public.
+        visibility: A Visibility enum containing the new visibility of this
+                    playlist.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
@@ -93,13 +86,12 @@ class Playlist:
         Return the playlist cover image.
 
         Returns:
-        A Status object containing the cover image url as a string.
+        A Response object containing the cover image url as a string.
         """
         pass
 
     # GET https://api.spotify.com/v1/playlists/{playlist_id}/tracks
-    # TODO num_tracks should be None - implying there is no limit
-    def tracks(self, start=0, num_tracks=-1):
+    def tracks(self, start=0, num_tracks=None):
         """
         Return one or more tracks in the playlist.
 
@@ -108,22 +100,21 @@ class Playlist:
         parameters.
 
         Optional Parameters:
-        start: An integer specifying the 0-indexed position of the first
-                  track to return. Position can be omitted to return tracks
-                  starting with the first song in the playlist.
-        num_tracks:    An integer specifying the number of tracks to return. Limit
-                  can be omitted to return as many tracks as are present at
-                  the start position.
+        start:      An integer specifying the 0-indexed position of the first
+                    track to return. Can be omitted to return tracks starting
+                    from the first song in the playlist.
+
+        num_tracks: An integer specifying the number of tracks to return. Can
+                    be omitted to return as many tracks as are present at
+                    start.
 
         Returns:
-        A Status object containing a list of Track objects.
+        A Response object containing a list of Track objects.
         """
         pass
 
-    # TODO api requires track position and track but shouldnt need both
-    # TODO should parameter information repeat general function information
-    # DELETE https://api.spotify.com/v1/playlists/{playlist_id}/tracks
     # TODO test this in practice, what does it actually mean? Nobody knows.
+    # DELETE https://api.spotify.com/v1/playlists/{playlist_id}/tracks
     def remove_tracks(self, tracks=None, positions=None):
         """
         Remove one or more tracks from the playlist.
@@ -131,50 +122,51 @@ class Playlist:
         When provided with a list of tracks, all occurrences of those tracks in
         the playlist will be removed. Providing a list of track positions will
         only remove the tracks in those positions from the playlist. No tracks
-        will be removed if neither of the parameters are provided.
+        will be removed if neither of the parameters are provided. If both
+        parameters are provided, positions takes precedence.
 
         Optional Parameters:
         tracks:    A list of Track objects to be removed from the playlist. All
                    occurrences of these tracks will be removed.
+
         positions: A list of integers specifying the 0-indexed positions of
                    tracks to be remoed from the playlist. Only tracks in these
                    positions will be removed.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
-    # TODO parameter naming
-    # TODO how to describe negative integer behavior
     # PUT https://api.spotify.com/v1/playlists/{playlist_id}/tracks
-    def reorder_tracks(self, src_index, dest_index, number=1):
+    def reorder_tracks(self, source_index, destination_index, number=1):
         """
         Move one or more tracks to another position in the playlist.
 
-        Moves the specified number of tracks starting at src_index to before the
-        track currently at destination. If number is unspecified, only one
-        track is moved.
+        Moves the specified number of tracks starting at source_index to before
+        the track currently at destination_index. If number is unspecified,
+        only one track is moved.
 
         Parameters:
-        src_index:   An integer specifying the 0-indexed position of the first
-                     track to be moved. A negative integer will be evaluated 
-                     from the end of the playlist as negative indices behave in
-                     lists. This must be a valid index into a list of length
-                     len(playlist).
-        dest_index:  An integer specifying the 0-indexed position of the track
-                     before which the other tracks will be moved. A negative
-                     integer will be evaluated from the end of the playlist as
-                     negative indices behave in lists. This must be a valid
-                     index into a list of length len(playlist).
+        source_index:      An integer specifying the 0-indexed position of the first
+                           track to be moved. A negative integer will be evaluated 
+                           from the end of the playlist as negative indices behave in
+                           lists. This must be a valid index into a list of length
+                           len(playlist).
+
+        destination_index: An integer specifying the 0-indexed position of the track
+                           before which the other tracks will be moved. A negative
+                           integer will be evaluated from the end of the playlist as
+                           negative indices behave in lists. This must be a valid
+                           index into a list of length len(playlist).
 
         Optional Parameters:
-        number:      A nonnegative integer specifying the number of tracks to
-                     be moved. Specifying 0 will result in no change to the
-                     playlist.
+        number:            A nonnegative integer specifying the number of tracks to
+                           be moved. Specifying 0 will result in no change to the
+                           playlist.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
@@ -188,24 +180,23 @@ class Playlist:
                 be the only tracks in the playlist.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
-    # TODO hard limit of 256 KB - we need to tell the user hard limits
-    # TODO the api wants a base64 encoded jpeg - should we just have filename
-    # and should we convert for them
     # PUT https://api.spotify.com/v1/playlists/{playlist_id}/images
     def replace_image(self, image):
         """
         Replace the playlist cover image.
 
+        The image must be a JPEG and can be at most 256 KB in size.
+
         Parameters:
         image: A string containing the filename of the image to use as the
-               playlist cover image. The image must be a JPEG.
+               playlist cover image. The image must be a JPEG up to 256 KB.
 
         Returns:
-        A Status object.
+        A Response object.
         """
         pass
 
@@ -216,3 +207,16 @@ class Playlist:
     def __len__(self):
         """Return the number of tracks in the playlist."""
         return len(self._raw['tracks']['items'])
+
+    class Visibility(Enum):
+        """
+        An Enum to describe the three possible playlist visibilities.
+
+        Playlists can be public, private but not collaborative, and private and
+        collaborative. Playlists cannot be collaborative without also being
+        private.
+        """
+
+        PUBLIC = 1
+        PRIVATE = 2
+        COLLABORATIVE = 3
