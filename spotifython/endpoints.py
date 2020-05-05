fbc4c00887ecf89601e03953f1fd750e9bd2018c
Spencer Yu <syu04012012@gmail.com>
2020-05-05 03:04:03 -0400

    implement all top level class methods except search, add corresponding endpoints

--- spotifython/endpoints.py
+++ spotifython/endpoints.py
@@ -1,6 +1,11 @@
 class Endpoints:
     BASE_URI = 'https://api.spotify.com'
-    GET_ALBUMS = '/v1/artists/{id}/albums'
+    SEARCH_GET_ALBUMS = '/v1/albums'
+    SEARCH_GET_ARTISTS = '/v1/artists'
+    SEARCH_GET_TRACKS = '/v1/tracks'
+    SERACH_GET_USER = '/v1/users/{}'
+    SEARCH_GET_CURRENT_USER = '/v1/me'
+    SEARCH_GET_PLAYLIST = '/v1/playlists/{}'
 
 # import endpoint
 # endpoint.BASE_URI        
