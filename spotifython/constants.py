1a24b6e26b67e9fdc7f0f6c41ec919631a54aa10
Niko Gupta <niko.gupta@gmail.com>
2020-05-21 10:57:23 -0400

    Rename Spotifython -> Session and move constants
    Constants now live in their own separate file, constants.py

---
+++ spotifython/constants.py
@@ -0,0 +1,26 @@
+# Enums
+TOKEN_REGION = 'from_token'
+ALBUM = 'album'
+ARTIST = 'artist'
+PLAYLIST = 'playlist'
+TRACK = 'track'
+SHOW = 'show'
+EPISODE = 'episode'
+REQUEST_GET = 'GET'
+REQUEST_PUT = 'PUT'
+REQUEST_DELETE = 'DELETE'
+REQUEST_POST = 'POST'
+USER = 'user'
+LONG = 'long'
+MEDIUM = 'medium'
+SHORT = 'short'
+CONTEXT = 'context'
+OFF = 'off'
+KEEP_PLAY_STATE = 'keep_play_state'
+FORCE_PLAY = 'force_play'
+PUBLIC = 'public'
+PRIVATE = 'private'
+PRIVATE_COLLAB = 'private_collab'
+
+# Constants
+DEFAULT_REQUEST_TIMEOUT = 10 # in seconds
