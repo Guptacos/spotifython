4037431dac095d22b28a4616b7325cd6774ec338
Niko Gupta <niko.gupta@gmail.com>
2020-05-31 11:29:32 -0400

    Remove Context tests for test_player

--- tests/test_player.py
+++ tests/test_player.py
@@ -159,60 +159,6 @@ class TestPlayer(unittest.TestCase):
         pass
 
 
-class TestContext(unittest.TestCase):
-
-
-    def test_init(self):
-        # Get objects
-        tracks = get_dummy_data(const.TRACKS, 5, True)
-        artist = get_dummy_data(const.ARTISTS, 1, True)[0]
-        album = get_dummy_data(const.ALBUMS, 1, True)[0]
-        playlist = get_dummy_data(const.PLAYLISTS, 1, True)[0]
-
-        # Should all pass
-        Context(tracks)
-        Context(artist)
-        Context(album)
-        Context(playlist)
-
-        # Should fail
-        self.assertRaises(TypeError, Context, tracks[0])
-        self.assertRaises(TypeError, Context, tracks + [artist])
-        self.assertRaises(ValueError, Context, tracks, -1)
-        self.assertRaises(ValueError, Context, tracks, 10)
-        self.assertRaises(ValueError, Context, tracks[:3], tracks[4])
-
-
-    def test_eq(self):
-        # Get objects
-        tracks = get_dummy_data(const.TRACKS, 5, True)
-        album = get_dummy_data(const.ALBUMS, 1, True)[0]
-
-        self.assertEqual(Context(album), Context(album))
-        self.assertEqual(Context(tracks), Context(tracks))
-        self.assertEqual(Context(tracks, 3), Context(tracks, 3))
-        self.assertNotEqual(Context(tracks, 2), Context(tracks, 3))
-        self.assertNotEqual(Context(album, 3), Context(tracks, 3))
-
-
-    def test_accessors(self):
-        # Test basic accessor equality
-        tracks = get_dummy_data(const.TRACKS, 5, True)
-        cont = Context(tracks, 4)
-        self.assertEqual(cont.item(), tracks)
-        self.assertEqual(cont.offset(), 4)
-
-        # Test passing a Track as offset
-        tracks = get_dummy_data(const.TRACKS, 5, True)
-        cont = Context(tracks, tracks[2])
-        self.assertEqual(cont.offset(), 2)
-
-        # Test a different type for item
-        album = get_dummy_data(const.ALBUMS, 1, True)[0]
-        cont = Context(album)
-        self.assertEqual(cont.item(), album)
-
-
 # This allows the tests to be executed
 if __name__ == '__main__':
     unittest.main()
@@ -220,4 +166,4 @@ if __name__ == '__main__':
 #pylint: disable=wrong-import-position
 #pylint: disable=wrong-import-order
 from spotifython.user import User
-from spotifython.player import Player, Context
+from spotifython.player import Player
