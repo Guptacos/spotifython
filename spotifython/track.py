2a617dff918e339348c203e460c13b1def7a70f7
Niko Gupta <niko.gupta@gmail.com>
2020-06-03 09:59:18 -0400

    Add new getter methods and implement Track class

--- spotifython/track.py
+++ spotifython/track.py
@@ -1,71 +1,305 @@
-from album import Album
-from artist import Artist
-from copy import deepcopy
+""" Track class
 
-AUDIO_FEATURES = 'GET https://api.spotify.com/v1/audio-features/%s'
-AUDIO_ANALYSIS = 'GET https://api.spotify.com/v1/audio-features/%s'
+This class represents a Track object, which represents a Spotify track / song.
+"""
+
+# Standard library imports
+import copy
+
+# Local imports
+import spotifython.constants as const
+from spotifython.endpoints import Endpoints
+import spotifython.utils as utils
 
 
 class Track:
-    # User should never call this constructor. As a result, they should never
-    # have access to the track_info structure prior to creating a track. This
-    # assumption lets us modify the track_info object willy-nilly.
-    def __init__(self, track_info: dict):
-        self._raw = deepcopy(track_info)
+    """ The Track class. Use methods here to get information about a Track.
 
-        self._album = Album(track_info.get('album', None))
-        self._artists = list()
-        for artist in track_info.get('artists', None):
-            self._artists.append(Artist(artist))
+    Required token scopes:
+        None: the methods in the Track class require a token, but the token
+            needs no scopes.
+    """
 
 
+    # TODO: should init call self._update_fields()?
+    def __init__(self, session, info):
+        """ Get an instance of Track
+
+        This constructor should never be called by the client. To get an
+        instance of Track, use Session.get_tracks()
+
+        Args:
+            session: a Spotifython instance
+            info: the track's information
+        """
+        # Validate inputs
+        if 'id' not in info:
+            raise ValueError('Track id not in info')
+
+        self._id = info['id']
+
+        # TODO: name this _raw or _info?
+        self._raw = copy.deepcopy(info)
+        self._session = session
+
+
+    # TODO: test this
+    # TODO: is it bad for print to make api calls?
+    # TODO: should include artists?
     def __str__(self):
-        return '%s by %s' % (self._raw['name'], self._artists[0].name())
+        name = self.name()
+        return f'Track {name}'
+
+
+    # Too long print?
+    def __repr__(self):
+        return str(self) + f' with id <{self.spotify_id()}>'
+
 
     def __eq__(self, other):
-        return isinstance(other, Track) and self._id == other._id
+        return utils.spotifython_eq(self, other)
 
-    # the following are getter methods to fetch relevant information.
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+    def __hash__(self):
+        return utils.spotifython_hash(self)
+
+
+    def __len__(self):
+        """ Get the length of the track
+
+        Returns:
+            int: the length of the track in milliseconds
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'duration_ms')
+
+
+    def _update_fields(self):
+        """ Update self._raw using the Track id
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+
+        response_json, status_code = utils.request(
+            session=self._session,
+            request_type=const.REQUEST_GET,
+            endpoint=Endpoints.TRACK_GET_DATA % self.spotify_id()
+        )
+
+        if status_code != 200:
+            raise utils.SpotifyError(status_code, response_json)
+
+        # Updates _raw with new values. One liner : for each key in union of
+        # keys in self._raw and response_json, takes value for key from
+        # response_json if present, else takes value for key from self._raw.
+        # TODO: this is weird notation, make a utility function for it.
+        # Especially useful since it is an action necessary for many classes.
+        self._raw = {**self._raw, **response_json}
+
+
+    def spotify_id(self):
+        """ Get the id of this Track
+
+        Returns:
+            str: the Track's Spotify id
+        """
+        return self._id
+
+
     def album(self):
-        # TODO: it might be worth caching ids to objects so we can easily
-        # reference pre-existing data.
-        return self._album
+        """ Get the Album this Track is on
+
+        Returns:
+            Album: a Spotifython Album object
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        album = utils.get_field(self, 'album')
+        return Album(self._session, album)
+
 
     def artists(self):
-        # TODO: it might be worth caching ids to objects so we can easily
-        # reference pre-existing data.
-        return self._artists
+        """ Get the Artists for this Track
+
+        Returns:
+            List[Artist]: a list of Spotifython Artist objects
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        artists = utils.get_field(self, 'artists')
+        return [Artist(self._session, art) for art in artists]
+
 
     def available_markets(self):
-        return self._raw['available_markets']
+        """ Get the country codes of the markets this Track is available in.
+
+        Returns:
+            List[str]: a list of the available country codes
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'available_markets')
+
 
     def name(self):
-        return self._raw['name']
+        """ Get the name of the Track
+
+        Returns:
+            str: the name of the Track as it appears on Spotify
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'name')
+
 
     def popularity(self):
-        return self._raw['popularity']
+        """ Get the popularity of the Track
+
+        The popularity is between 0 and 100 (inclusive), with 100 being the most
+        popular. This number is calculated by a Spotify algorithm.
+
+        Returns:
+            int: the popularity of the Track as calculated by Spotify
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'popularity')
+
+
+    def disc_number(self):
+        """ Get the Track's disc number (1 indexed)
+
+        The number of the disc on which this Track appears. This is usually 1,
+        unless an album has multiple discs.
+
+        Returns:
+            int: the number of the Track's disc
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'disc_number')
+
 
     def track_number(self):
-        return self._raw['track_number']
+        """ Get the Track's number
 
-    # TODO: for debugging implementation purposes only, user should not call.
-    def _asdict(self):
-        return deepcopy(self._raw)
+        This is the track's number in the album. If an album has several discs,
+        the track number is the number of this Track on the disc it appears.
 
-    # These are the API wrapper calls that track objects should be concerned
-    # with.
+        Returns:
+            int: the Track's number on it's disc
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'track_number')
+
+
+    def explicit(self):
+        """ Get whether the Track is explicit
+
+        Returns:
+            bool: True if the track has explicit lyrics, and False otherwise
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'explicit')
+
+
+    def uri(self):
+        """ Get the Track's uri
+
+        Returns:
+            str: the Spotify uri for this Track
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'uri')
+
+
+    def href(self):
+        """ Get the Track's href
+
+        Returns:
+            str: a link to the Web API endpoint providing full track details.
+
+        Calls endpoints:
+            GET     /v1/tracks/{id}
+        """
+        return utils.get_field(self, 'href')
+
+
+    # TODO: maybe change this to a feature object?
     def audio_features(self):
-        if 'id' not in self._raw:
-            return None
-        track_id = self._raw.get['id']
-        request = AUDIO_FEATURES % (track_id)
-        # TODO: send the actual request
-        return
+        """ Get the audio features for this Track
 
+        For more information on Track audio features, see here:
+        https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
+
+        Returns:
+            dict: a dictionary as defined at the above link, where the key is
+                the audio feature, and the value is the value of that feature.
+
+        Calls endpoints:
+            GET     /v1/audio-features/{id}
+        """
+        response_json, status_code = utils.request(
+            session=self._session,
+            request_type=const.REQUEST_GET,
+            endpoint=Endpoints.TRACK_FEATURES % self.spotify_id()
+        )
+
+        if status_code != 200:
+            raise utils.SpotifyError(status_code, response_json)
+
+        return response_json
+
+
+    # TODO: this is a fat response json... should we make an analysis object?
     def audio_analysis(self):
-        if 'id' not in self._raw:
-            return None
-        track_id = self._raw.get['id']
-        request = AUDIO_ANALYSIS % (track_id)
-        # TODO: send the actual request
-        return
+        """ Get the audio analysis for this Track
+
+        The Audio Analysis describes the track’s structure and musical content,
+        including rhythm, pitch, and timbre. All information is precise to the
+        audio sample.
+
+        For more information on Track audio analysis, see here:
+        https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/
+
+        Returns:
+            dict: a dictionary containing the audio analysis as defined at the
+                above link.
+
+        Calls endpoints:
+            GET     /v1/audio-analysis/{id}
+        """
+        response_json, status_code = utils.request(
+            session=self._session,
+            request_type=const.REQUEST_GET,
+            endpoint=Endpoints.TRACK_ANALYSIS % self.spotify_id()
+        )
+
+        if status_code != 200:
+            raise utils.SpotifyError(status_code, response_json)
+
+        return response_json
+
+
+#pylint: disable=wrong-import-position
+from spotifython.album import Album
+from spotifython.artist import Artist
