14a4867e0437352c05f4a8d6558809b3830cc761
Niko Gupta <niko.gupta@gmail.com>
2020-06-04 13:24:25 -0400

    Create skeleton of Album methods and endpoints

--- spotifython/album.py
+++ spotifython/album.py
@@ -1,31 +1,74 @@
-from track import Track
-from artist import Artist
+""" Album class
+
+This class represents an Album object, which represents a Spotify album.
+"""
+
+# Standard library imports
+import copy
+
+# Local imports
+import spotifython.constants as const
+from spotifython.endpoints import Endpoints
+import spotifython.utils as utils
 
 
 class Album:
-    def __init__(self, album_info):
-        self._album_type = album_info.get('album_type', '')
-        self._artists = list()
-        for artist in album_info.get('artists', []):
-            self._artists.append(Artist(artist))
+    """ The Album class. Use methods here to get information about an Album.
 
-        tracks_wrapper = album_info.get('tracks', None)
-        if tracks_wrapper != None:
-            self._tracks = list()
-            for track in tracks_wrapper.get('items', []):
-                self._tracks.append(Track(track))
+    Required token scopes:
+        None: the methods in the Album class require a token, but the token
+            needs no scopes.
+    TODO: is this true?
+    """
 
-        self._available_markets = album_info.get('available_markets', [])
-        self._album_id = album_info.get('id', '')
-        self._images = album_info.get('images', {})
-        self._name = album_info.get('name', '')
-        self._popularity = album_info.get('popularity', -1)
-        self._release_date = album_info.get('release_date', '')
 
+    def __init__(self, session, info):
+        """ Get an instance of Album
+
+        This constructor should never be called by the client. To get an
+        instance of Album, use Session.get_albums()
+
+        Args:
+            session: a Session instance
+            info: the album's information
+        """
+        # Validate inputs
+        if 'id' not in info:
+            raise ValueError('Album id not in info')
+
+        self._id = info['id']
+
+        self._raw = copy.deepcopy(info)
+        self._session = session
+
+        # TODO:?????
         self._iter = 0
 
+
+    def __str__(self):
+        pass
+
+
+    def __repr__(self):
+        pass
+
+
     def __eq__(self, other):
-        return isinstance(other, Album) and self._album_id == other._album_id
+        pass
+
+
+    def __ne__(self, other):
+        pass
+
+
+    def __hash__(self):
+        pass
+
+
+    def __len__(self):
+        # return len(self._tracks)
+        pass
+
 
     # iter and next let you loop through the tracks in the album. len gives you
     # the number of tracks in the album.
@@ -33,42 +76,101 @@ class Album:
         self._iter = 0
         return self
 
-    def __next__(self):
-        if self._iter < len(self._tracks):
-            track = self._tracks[self._iter]
-            self._iter += 1
-            return track
-        else:
-            raise StopIteration
 
-    def __len__(self):
-        return len(self._tracks)
+    def __next__(self):
+        # if self._iter < len(self._tracks):
+        #     track = self._tracks[self._iter]
+        #     self._iter += 1
+        #     return track
+
+        # raise StopIteration
+        pass
+
+
+    def __reversed__(self):
+        pass
+
+
+    def __contains__(self, other):
+        pass
+
+
+    def __getitem__(self, key):
+        # Should accept negative keys and slice objects
+        pass
+
+
+    def spotify_id(self):
+        return self._id
+
+
+    def type(self):
+        pass
+
 
     def artists(self):
-        return self._artists
+        # for artist in album_info.get('artists', []):
+        #     self._artists.append(Artist(artist))
+        # return self._artists
+        pass
 
-    def tracks(self):
-        return self._tracks
-
-    def album_type(self):
-        return self._album_type
 
     def available_markets(self):
-        return self._available_markets
+        pass
 
-    def album_id(self):
-        return self._album_id
 
-    # TODO: usually has three sizes, maybe take in an optional arg for size,
-    # otherwise return the first one (largest).
-    def image(self, size=None):
-        return self._images[0] if size == None else self._images[0]
+    def copyrights(self):
+        pass
+
+
+    def genres(self):
+        pass
+
+
+    def href(self):
+        pass
+
+
+    def uri(self):
+        pass
+
+
+    def images(self):
+        """
+        # TODO: usually has three sizes, maybe take in an optional arg for size,
+        # otherwise return the first one (largest).
+        def image(self, size=None):
+            return self._images[0] if size == None else self._images[0]
+        """
+        pass
+
+
+    def label(self):
+        pass
+
 
     def name(self):
-        return self._name
+        pass
+
 
     def popularity(self):
-        return self._popularity
+        pass
+
 
     def release_date(self):
-        return self._release_date
+        pass
+
+
+    def tracks(self):
+        # tracks_wrapper = album_info.get('tracks', None)
+        # if tracks_wrapper != None:
+        #     self._tracks = list()
+        #     for track in tracks_wrapper.get('items', []):
+        #         self._tracks.append(Track(track))
+        # return self._tracks
+        pass
+
+
+#pylint: disable=wrong-import-position
+from spotifython.track import Track
+from spotifython.artist import Artist
