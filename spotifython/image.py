0b1f28860dd786a5227be7c3358f45996af88de2
Niko Gupta <niko.gupta@gmail.com>
2020-06-08 13:04:46 -0400

    Create Image class
    This class should be used to represent images such as User profile
    pictures, Album cover art, etc.

---
+++ spotifython/image.py
@@ -0,0 +1,53 @@
+""" Image class """
+
+# Standard library imports
+import copy
+
+
+class Image:
+    """ Container class representing a Spotify image
+
+    Use methods here to get information about an Image
+    """
+
+    def __init__(self, info):
+        """ Get an instance of Image
+
+        This constructor should never be called by the client. To get an
+        instance of Image, use Album.images() or User.image()
+
+        Args:
+            info: the image's information
+        """
+        if 'url' not in info:
+            raise ValueError('Image class init with no url')
+
+        self._raw = copy.deepcopy(info)
+
+
+    def __str__(self):
+        return self._raw['url']
+
+
+    def __repr__(self):
+        return str(self)
+
+
+    def width(self):
+        """ Get the width of the image in pixels
+
+        Returns:
+            (int): the width in pixels, if known
+            None: if the width is not known
+        """
+        return self._raw.get('width', None)
+
+
+    def height(self):
+        """ Get the height of the image in pixels
+
+        Returns:
+            (int): the height in pixels, if known
+            None: if the height is not known
+        """
+        return self._raw.get('height', None)
