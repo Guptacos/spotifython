760dbd958c47c4d8b21d3f252d6933f4411a13b2
Spencer Yu <syu04012012@gmail.com>
2020-06-08 08:39:56 -0700

    Artist Class Implementation and Unit Test Setup (#31)
    * implement artist class. not yet tested.
    
    * question about valueError
    
    * Revert "question about valueError"
    
    This reverts commit d3225c909a31539cd2c581248d4071d2f8224de0.
    
    * question about market as ValueError
    
    * update endpoints to consts
    
    * update search limit for artist.albums()
    
    * Update README.md
    
    * Update artist.py
    
    * Update artist.py
    
    * Update artist.py
    
    * artist.py compiles, moved spotifython constants to separate class to avoid compilation issues
    
    * move constants back into spotifython top level, fix import naming
    
    * update partial failure doc
    
    * implement artist class. not yet tested.
    
    * question about valueError
    
    * Revert "question about valueError"
    
    This reverts commit d3225c909a31539cd2c581248d4071d2f8224de0.
    
    * question about market as ValueError
    
    * update endpoints to consts
    
    * update search limit for artist.albums()
    
    * Update README.md
    
    * Update artist.py
    
    * Update artist.py
    
    * Update artist.py
    
    * artist.py compiles, moved spotifython constants to separate class to avoid compilation issues
    
    * move constants back into spotifython top level, fix import naming
    
    * update partial failure doc
    
    * remove type hints and specify arg type in the function docstring
    
    * remove lingering sp.XXX calls
    
    * rearrange imports
    
    * add class level description
    
    * type checking using isinstance
    
    * remove response class usage
    
    * rename endpoint to endpoints
    
    * get rid of .contents()
    
    * limit lines to 80 characters, change single quote docstring to double quote
    
    * resolve the majority of pylint's complaints
    
    * add required token scopes
    
    * add utils for getting fields, address niko's pr comments
    
    * fix syntax errs
    
    * update utils
    
    * update utils to include get field, fix linting errors
    
    * update lint for utils
    
    * refactor artist to use paginate_get
    
    * add repr, eq, ne, hash
    
    * bandaid fix to make unit tests run
    
    * add template for artist tests
    
    * artist tests compile
    
    * fix isinstance usages where python throws error, informative exception messages
    
    * address niko's comments
    
    * undo changes to other files
    
    * add endpoint from get by id to endpoints, remove unnecessary linting ignore, rename session to _session
    
    * first line spacing in util functions, rename artist_info to info
    
    Co-authored-by: Adithya Raghuraman <adithyaraghuraman98@gmail.com>

--- spotifython/artist.py
+++ spotifython/artist.py
@@ -1,26 +1,64 @@
-from album import Album
-from track import Track
-from copy import deepcopy
-from response import Response
-import spotifython as sp
+""" Artist class
+
+This class represents an Artist object, tied to a Spotify user id.
+"""
+
+# Local imports
+import spotifython.constants as const
+from spotifython.endpoints import Endpoints
+import spotifython.utils as utils
+
+# pylint: disable = pointless-string-statement, too-many-instance-attributes
+# pylint: disable = too-many-branches
 
 class Artist:
-    # User should never call this constructor. As a result, they should never
-    # have access to the artist_info structure prior to creating an Artist.
-    def __init__(self, artist_info: dict, top: Spotifython):
-        self._top = top
-        self._raw = artist_info
-        self.albums = None # Lazily load
-        self.top_tracks = None # Lazily load
-        self.related_artists = None # Lazily load
+    """ Artist class
+
+    This class represents an Artist object, tied to a Spotify user id.
+    """
+
+    def __init__(self, session, info):
+        """ User should never call this constructor. As a result, they should
+        never have access to the artist_info structure prior to creating an
+        Artist.
+
+        Args:
+            session: a Spotifython session instance
+            artist_info: a dictionary containing known values about the artist
+        """
+        # TODO: add type checking for session
+        if not isinstance(info, dict):
+            raise TypeError('artist_info should be dict')
+
+        self._session = session
+        self._raw = info
+        # Lazily loaded fields from API calls
+        self._albums = None
+        self._top_tracks = None
+        self._related_artists = None
+        # Params used to call each previous entry - determins whether to reload
+        self._albums_query_params = None
+        self._top_tracks_query_params = None
+        self._related_artists_query_params = None
 
     ##################################
     # Overloads
     ##################################
 
     def __str__(self):
-        c = self.name()
-        return c.contents() if c else 'ERROR'
+        return self.name()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return utils.spotifython_eq(self, other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return utils.spotifython_hash(self)
 
     ##################################
     # Field accessors
@@ -32,110 +70,282 @@ class Artist:
     # Images: unsupported object
     # Type: is an artist. we don't need to include this.
 
-    def genres(self) -> Response: # List[str]
-        return Response(status=Response.OK, contents=self._raw.get('genres'))
+    def genres(self):
+        """ Getter for the genre of an artist.
 
-    def href(self) -> Response: # str
-        return Response(status=Response.OK, contents=self._raw.get('href'))
-    
-    def artist_id(self) -> Response: # str
-        return Response(status=Response.OK, contents=self._raw.get('id'))
-    
-    def name(self) -> Response: # str
-        return Response(status=Response.OK, contents=self._raw.get('name'))
-    
-    def popularity(self) -> Response: # int
-        return Response(status=Response.OK, contents=self._raw.get('popularity'))
-    
-    def uri(self) -> Response: # str
-        return Response(status=Response.OK, contents=self._raw.get('uri'))
+        Returns:
+            A List[str] of the genres associated with the artist.
+        """
+        return utils.get_field(self, 'genres')
+
+    def href(self):
+        """ Getter for the href for an artist.
+
+        Returns:
+            A str of the Web API endpoint providing full details of the artist.
+        """
+        return utils.get_field(self, 'href')
+
+    def spotify_id(self):
+        """ Getter for the spotify id for an artist.
+
+        Returns:
+            A str of the artist's Spotify ID.
+        """
+        return utils.get_field(self, 'id')
+
+    def name(self):
+        """ Getter for the name for an artist.
+
+        Returns:
+            A str of the artist name.
+        """
+        return utils.get_field(self, 'name')
+
+    def popularity(self):
+        """ Getter for the popularity for an artist.
+
+        Returns:
+            An int of the popularity of the artist.
+        """
+        return utils.get_field(self, 'popularity')
+
+    def uri(self):
+        """ Getter for the uri for an artist.
+
+        Returns:
+            A str of the Spotify URI for the artist.
+        """
+        return utils.get_field(self, 'uri')
+
+    def _update_fields(self):
+        """ If field is not present, update it using the object's artist id.
+
+        Raises:
+            ValueError if artist id not present in the raw object data.
+
+        Required token scopes:
+            None
+
+        Calls endpoints:
+            GET     /v1/artists/{id}
+        """
+        endpoint = Endpoints.ARTIST_GET_BY_ID % self.spotify_id()
+        response_json, status_code = utils.request(
+            session=self._session,
+            request_type=const.REQUEST_GET,
+            endpoint=endpoint,
+        )
+
+        if status_code != 200:
+            raise utils.SpotifyError(status_code, response_json)
+
+        # Updates _raw with new values. One liner : for each key in union of
+        # keys in self._raw and response_json, takes value for key from
+        # response_json if present, else takes value for key from self._raw.
+        # TODO: this is weird notation, make a utility function for it.
+        # Especially useful since it is an action necessary for many classes.
+        self._raw = {**self._raw, **response_json}
 
     ##################################
     # API Calls
     ##################################
-    
-    def albums(self, 
-        search_limit: int = None,
-        include_groups: str = None,
-        country: str = sp.TOKEN_REGION,
-    ) -> Response: # List[Album]
-        '''
-        Gets the albums associated with the current Spotify artist.
-        
+
+    def albums(self,
+               search_limit=None,
+               include_groups=None,
+               market=const.TOKEN_REGION
+               ):
+        """ Gets the albums associated with the current Spotify artist.
+
         Args:
-            search_limit: (Optional) the maximum number of results to return.
-            include_groups: (Optional) A comma-separated list of keywords 
-                that will be used to filter the response. If not supplied, 
-                all album types will be returned. 
-                Valid values are: 'album', 'single', 'appears_on', 'compilation' 
-            country: (Optional) An ISO 3166-1 alpha-2 country code or the string sp.TOKEN_REGION.
-                Supply this parameter to limit the response to one particular geographical 
-                market. If this value is None, results will be returned for all countries and you 
-                are likely to get duplicate results per album, one for each country in 
-                which the album is available!
+            search_limit: (Optional) int, the maximum number of results
+                to return.
+            include_groups: (Optional) List[str], a list of keywords
+                that will be used to filter the response. If not supplied,
+                all album types will be returned.
+                Valid values are: sp.ARTIST_ALBUM, sp.ARTIST_SINGLE,
+                sp.ARTIST_APPEARS_ON, sp.ARTIST_COMPILATION
+            market: (Optional) str, An ISO 3166-1 alpha-2 country code or the
+                string sp.TOKEN_REGION.
+                Supply this parameter to limit the response to one particular
+                geographical market. If this value is None, results will be
+                returned for all countries and you are likely to get duplicate
+                results per album, one for each country in which the album
+                is available!
 
-        Returns: 
-            A response object containing a list of Albums if the request succeeded.
+        Returns:
+            A List[Album] if the request succeeded.
 
-        Exceptions:
+        Raises:
             TypeError for invalid types in any argument.
-        
+            ValueError for invalid market.
+                TODO: is this even necessary, will raise ex
+            HTTPError for web request errors or partial failures.
+
+        Required token scopes:
+            None
+
         Calls endpoints:
             GET	/v1/artists/{id}/albums
-        '''
+        """
 
-        # search_limit -> limit: int = None, offset: int = None
-        # Request: the response body contains an array of simplified album
-        # objects (wrapped in a paging object)
-        return self.albums
-    
-    
+        # Type validation
+        if search_limit is not None and not isinstance(search_limit, int):
+            raise TypeError('search_limit should be None or int')
+        if include_groups is not None and \
+            not all(isinstance(x, str) for x in include_groups):
+            raise TypeError('include_groups should be None or str')
+        if market is not None and not isinstance(market, str):
+            raise TypeError('market should be None or str')
+
+        # Lazy loading check
+        search_query = (search_limit, include_groups, market)
+        if search_query == self._albums_query_params:
+            return self._albums
+
+        # Construct params for API call
+        endpoint = Endpoints.ARTIST_GET_ALBUMS % self.spotify_id()
+        uri_params = dict()
+        if include_groups is not None and len(include_groups) > 0:
+            uri_params['include_groups'] = ','.join(include_groups)
+        if market is not None:
+            uri_params['market'] = market
+
+        # Update stored params for lazy loading
+        self._albums = utils.paginate_get(session=self._session,
+                                          limit=search_limit,
+                                          return_class=Album,
+                                          endpoint=endpoint,
+                                          uri_params=uri_params
+                                          )
+        self._albums_query_params = search_query
+
+        return self._albums
+
     def top_tracks(self,
-        country: str = sp.TOKEN_REGION,
-        search_limit: int = 10,
-    ) -> Response: # List[Track]
-        '''
-        Gets the top tracks associated with the current Spotify artist.
-        
+                   market=const.TOKEN_REGION,
+                   search_limit=10,
+                   ):
+        """ Gets the top tracks associated with the current Spotify artist.
+
         Args:
-            country: An ISO 3166-1 alpha-2 country code or the string sp.TOKEN_REGION.
-            search_limit: (Optional) the maximum number of results to return.
+            market: str, an ISO 3166-1 alpha-2 country code or the string
+                sp.TOKEN_REGION.
+            search_limit: int, the maximum number of results to return.
 
-        Returns: 
-            A response object containing a list of Tracks if the request succeeded.
+        Returns:
+            A List[Track] if the request succeeded.
 
-        Exceptions:
+        Raises:
             TypeError for invalid types in any argument.
-            ValueError for invalid country, or if country is None.
-            ValueError if search_limit is > 10: this is the Spotify API's search limit.
-        
+            ValueError if market is None.
+            ValueError for invalid market.
+                TODO: is this even necessary, will raise ex
+            ValueError if search_limit is > 10: this is the Spotify API's search
+                limit.
+            HTTPError for web request errors.
+
+        Required token scopes:
+            None
+
         Calls endpoints:
             GET	/v1/artists/{id}/top-tracks
-        '''
+        """
 
-        # This search limit is not part of the API, spotify always returns up to 10.
-        return self.top_tracks
-    
+        # Note: This search limit is not part of the API, Spotify always returns
+        # up to 10. Market query param is 'country' in the API but named marked
+        # for consistency
+
+        # Type validation
+        if not isinstance(market, str):
+            raise TypeError('market should bes tr')
+        if not isinstance(search_limit, int):
+            raise TypeError('search_limit should be int')
+
+        # Argument validation
+        if market is None:
+            raise ValueError('market is a required argument')
+        if search_limit < 0 or search_limit > 10:
+            raise ValueError('search_limit should be >= 0 and <= 10')
+
+        # Save params for lazy loading check
+        search_query = (market, search_limit)
+
+        # Construct params for API call
+        endpoint = Endpoints.ARTIST_TOP_TRACKS % self.spotify_id()
+        uri_params = dict()
+        uri_params['country'] = market
+
+        # Lazy loading check
+        if search_query == self._top_tracks_query_params:
+            return self._top_tracks
+
+        # Update stored params for lazy loading
+        self._top_tracks = utils.paginate_get(session=self._session,
+                                              limit=search_limit,
+                                              return_class=Track,
+                                              endpoint=endpoint,
+                                              uri_params=uri_params
+                                              )
+        self._top_tracks_query_params = search_query
+
+        return self._top_tracks
+
     def related_artists(self,
-        search_limit: int = 20,
-    ) -> Response: # List[Artist]
-        '''
-        Gets Spotify catalog information about artists similar to a given artist.
+                        search_limit=20,
+                        ):
+        """ Gets Spotify catalog information about artists similar to a
+        given artist.
 
         Args:
-            search_limit: (Optional) the maximum number of results to return.
+            search_limit: (Optional) int, the maximum number of results
+                to return.
 
-        Returns: 
-            A response object containing a list of Artist objects 
-            if the request succeeded.
+        Returns:
+            A response object containing a List[Artist] if the request
+            succeeded.
 
-        Exceptions:
+        Raises:
             TypeError for invalid types in any argument.
-            ValueError if search_limit is > 20: this is the Spotify API's search limit.
-        
+            ValueError if search_limit is > 20: this is the Spotify API's
+                search limit.
+            HTTPError for web request errors.
+
+        Required token scopes:
+            None
+
         Calls endpoints:
             GET	/v1/artists/{id}/related-artists
-        '''
+        """
 
-        # This search limit is not part of the API, spotify always returns up to 20.
-        return self.related_artists
+        # This search limit is not part of the API, Spotify always returns up to
+        # 20.
+
+        # Type validation
+        if search_limit is not None and not isinstance(search_limit, int):
+            raise TypeError('search_limit should be None or int')
+
+        # Argument validation
+        if search_limit < 0 or search_limit > 20:
+            raise ValueError('search_limit should be >= 0 and <= 20')
+
+        # Save params for lazy loading check
+        search_query = (search_limit)
+
+        # Construct params for API call
+        endpoint = Endpoints.ARTIST_RELATED_ARTISTS % self.spotify_id()
+
+        # Lazy loading check
+        if search_query == self._related_artists_query_params:
+            return self._related_artists
+
+        # Update stored params for lazy loading
+        self._related_artists = utils.paginate_get(session=self._session,
+                                                   limit=search_limit,
+                                                   return_class=Artist,
+                                                   endpoint=endpoint
+                                                   )
+        self._related_artists_query_params = search_query
+
+        return self._related_artists
--- spotifython/endpoints.py
+++ spotifython/endpoints.py
@@ -1,8 +1,11 @@
-# import endpoint
-# endpoint.BASE_URI
 class Endpoints:
     BASE_URI = 'https://api.spotify.com'
-    GET_ALBUMS = '/v1/artists/{id}/albums'
+
+    # Artist endpoints
+    ARTIST_GET_ALBUMS = '/v1/artists/%s/albums'
+    ARTIST_TOP_TRACKS = '/v1/artists/%s/top-tracks'
+    ARTIST_RELATED_ARTISTS = '/v1/artists/%s/related-artists'
+    ARTIST_GET_BY_ID = '/v1/artists/%s'
 
     # Playlist
     PLAYLIST = '/v1/playlists/{playlist_id}'
