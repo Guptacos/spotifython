8a4b4f797a58731b26590d24e431e8c71245fd74
Rama Mannava <rama.mannava@yahoo.com>
2020-06-07 21:44:54 -0400

    address pull request changes

--- spotifython/endpoints.py
+++ spotifython/endpoints.py
@@ -1,9 +1,9 @@
 class Endpoints:
     BASE_URI = 'https://api.spotify.com'
     GET_ALBUMS = '/v1/artists/{id}/albums'
-    PLAYLIST = '/v1/playlists/{playlist_id}'
-    PLAYLIST_TRACKS = '/v1/playlists/{playlist_id}/tracks'
-    PLAYLIST_IMAGES = '/v1/playlists/{playlist_id}/images'
+    PLAYLIST = '/v1/playlists/%s'
+    PLAYLIST_TRACKS = '/v1/playlists/%s/tracks'
+    PLAYLIST_IMAGES = '/v1/playlists/%s/images'
 
 # import endpoint
 # endpoint.BASE_URI
