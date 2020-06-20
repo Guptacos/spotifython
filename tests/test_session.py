a01d48147e4e0eb74f17cb3d47d4115529136527
Spencer Yu <syu04012012@gmail.com>
2020-06-19 19:55:49 -0700

    getters

--- tests/test_session.py
+++ tests/test_session.py
@@ -40,10 +40,6 @@ class TestArtist(unittest.TestCase):
     # This function is called before every test_* function. Anything that is
     # needed by every test_* function should be stored in "self" here.
     def setUp(self):
-        # Note: since we're mocking Spotify and never actually using the token,
-        # we can put any string here for the token.
-        self.session = Session(TOKEN)
-
         # Mock the sp._request method so that we never actually reach Spotify
         self.patcher = patch.object(utils, 'request', autospec=True)
 
@@ -54,35 +50,50 @@ class TestArtist(unittest.TestCase):
         # Create the actual mock object
         self.request_mock = self.patcher.start()
 
-        # Logging can be added using print() now
-        self.stream_handler = logging.StreamHandler(sys.stdout)
-        logger.addHandler(self.stream_handler)
-
     # This function is called after every test_* function. Use it to clean up
     # any resources initialized by the setUp function. Only include it if you
     # actually need to clean up resources.
     def tearDown(self):
-        logger.removeHandler(self.stream_handler)
+        pass
 
     # Test __str__, __repr__
-    @unittest.skip('Not yet implemented')
     def test_str_overloads(self):
-        self.assertTrue(self.session == self.session)
+        # Note: since we're mocking Spotify and never actually using the token,
+        # we can put any string here for the token.
+        session = Session(TOKEN)
+        session_1 = Session(TOKEN + TOKEN)
+        self.assertEqual(str(session), str(session))
+        self.assertFalse(str(session) == str(session_1))
+        self.assertEqual(repr(session), repr(session))
+        self.assertFalse(repr(session) == repr(session_1))
 
     # Test __eq__, __ne__, __hash__
-    @unittest.skip('Not yet implemented')
     def test_equality_overloads(self):
-        self.assertTrue(False)
+        session = Session(TOKEN)
+        session_1 = Session(TOKEN + TOKEN)
+        self.assertEqual(session, session)
+        self.assertFalse(session == session_1)
+        self.assertEqual(hash(session), hash(session))
+        self.assertFalse(hash(session) == hash(session_1))
 
     # Test reauthenticate
-    @unittest.skip('Not yet implemented')
     def test_reauthenticate(self):
-        self.assertTrue(False)
+        TOKEN_1 = TOKEN + TOKEN
+        session = Session(TOKEN)
+        session.reauthenticate(TOKEN)
+        session_1 = Session(TOKEN_1)
+        self.assertFalse(session == session_1)
 
     # Test token, timeout
-    @unittest.skip('Not yet implemented')
     def test_getters(self):
-        self.assertTrue(False)
+        TOKEN_1 = TOKEN + TOKEN
+        session = Session(TOKEN)
+        session_1 = Session(TOKEN1)
+        self.assertEqual(token(session), token(session))
+        self.assertFalse(token(session) == token(session_1))
+        # Using default timeout
+        self.assertEqual(timeout(session), timeout(session))
+        self.assertEqual(timeout(session) == timeout(session_1))
 
     # Test search
     @unittest.skip('Not yet implemented')
@@ -126,4 +137,4 @@ from spotifython.player import Player
 from spotifython.playlist import Playlist
 from spotifython.track import Track
 from spotifython.user import User
-from spotifython.session import Session as sp
+from spotifython.session import Session
